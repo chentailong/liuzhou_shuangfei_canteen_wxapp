b8e9e5b94e3c75c246617f6811d8fa0bfb33d313
Longz~ <1561445463@qq.com>
2023-09-03 17:15:24 +0800

    订单列表页

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 //可改动页面
 // var domain = 'https://bsfy.gxxqy.cn/service'
-var domain = 'http://192.168.1.56:8089' //统一接口域名，测试环境
+var domain = 'http://localhost:8089' //统一接口域名，测试环境
 // var domain = 'http://192.168.1.101:8189' //统一接口域名，测试环境
 exports.domain = domain
